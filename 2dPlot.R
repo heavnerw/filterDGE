2e815a8d2505216c6b2000f64f6cf2b32c42cf74
Whitney <whitney.heavner@gmail.com>
2017-10-14 11:12:55 -0700

    Initialized local filterDGE repository

---
+++ 2dPlot.R
@@ -0,0 +1,17 @@
+library("dplyr")
+library("magrittr")
+library("graphics")
+library("ggplot2")
+library("rJava")
+
+setwd("~/Documents/Results/ul_v_dl/e13_v_e17/ATAC/filtered/")
+
+d2=read.table("e17Only_binomialTest_foldChange.txt", sep="\t", col.names=c("gene", "binomial_pVal", "qVal", "log2_foldChange"), stringsAsFactors=F)
+str(d2)
+
+plot(d2$log2_foldChange, -log(d2$binomial_pVal), main="ATACseq v RNAseq", xlab="Sleuth log2(fold change)", ylab="-log(ATACseq binomial pValue)", pch=19)
+
+
+text(d2$log2_foldChange, -log(d2$binomial_pVal), labels=d2$gene, cex=0.7, pos=4, col="red")
+
+d2 %>% filter(log2_foldChange >= 0) %>% use_series(gene) %>% n_distinct(na.rm=TRUE)
