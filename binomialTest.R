8f7597af15236461d1e77cb912796ea351755b86
Whitney <whitney.heavner@gmail.com>
2017-10-15 12:51:50 -0700

    Added script for two-sided binomial test

---
+++ binomialTest.R
@@ -0,0 +1,16 @@
+library("dplyr")
+library("magrittr")
+library("graphics")
+library("ggplot2")
+library("rJava")
+
+setwd("~/results/screen/")
+
+d1=read.table("test1_withRegDomainsP.txt", sep="\t", col.names=c("gene", "size", "peaks", "probability"), stringsAsFactors=F)
+str(d1)
+
+test <- function(x, n, p) {binom.test(x, n, p, alternative="two.sided")}
+counts <- mapply(test, d1$peaks, 9969, d1$probability)
+str(counts)
+
+counts %>% write.table(file="counts.txt")
